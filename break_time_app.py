71abd407ecb0a53a3140a1e262d53b4781ae4e99
Jannaee <jannaee@gmail.com>
2018-09-03 22:16:28 -0400

    Answer with instructors notes

--- break_time_app.py
+++ break_time_app.py
@@ -3,12 +3,14 @@ import webbrowser
 #imports allow python to know bring in open a web browser
 
 
-count = 0
-print ("Starting...")
-while (count < 3):
-    time.sleep(5)
+total_breaks = 3
+break_count = 0
+
+print ("Starting..." + time.ctime())
+while (break_count < 3):
+    time.sleep(5)#program reads for 5 seconds and run 3 times
     webbrowser.open("https://www.youtube.com/watch?v=5txHGxJRwtQ")
-    count += 1
+    break_count += 1
 
 print ("Done son")
 
