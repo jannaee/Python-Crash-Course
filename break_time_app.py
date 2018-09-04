994bf3be201e25056c49c464f3fca0cec25f62e8
Jannaee <jannaee@gmail.com>
2018-09-03 22:09:57 -0400

    My first python program

---
+++ break_time_app.py
@@ -0,0 +1,16 @@
+import time
+import webbrowser
+#imports allow python to know bring in open a web browser
+
+
+count = 0
+print ("Starting...")
+while (count < 3):
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=5txHGxJRwtQ")
+    count += 1
+
+print ("Done son")
+
+
+    
