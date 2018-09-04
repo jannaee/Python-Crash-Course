7481301989b7e0c0eefbc0ee55d7bca80047d2ab
Jannaee <jannaee@gmail.com>
2018-09-04 00:17:47 -0400

    renaming files

--- secret-message.py
+++ secret-message.py
@@ -1,3 +1,16 @@
-import os.listdir
+import os
 
-os.listdir("./secret-message-pics/prank")
+def rename_files():
+    #1 get file names from a folder
+    fileslist = os.listdir(r"C:\Users\Jannaee\Desktop\~GitProjects\Python-Crash-Course\secret-message-pics\new")
+    print (fileslist)# r is placed in front of the string becuase it tells python not to interpret the string in any other way but to take it as it is
+    saved_path = os.getcwd()
+    print("Current Working Directory is "+saved_path)
+    os.chdir(r"C:\Users\Jannaee\Desktop\~GitProjects\Python-Crash-Course\secret-message-pics\new")
+    #2 remame each file
+    for file_name in fileslist:
+        translation_table = str.maketrans("0123456789","          ","0123456789")
+        os.rename(file_name, fileslist.translate(translation_table))
+    os.chdir(saved_path)
+
+rename_files()
